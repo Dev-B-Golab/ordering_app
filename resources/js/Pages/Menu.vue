fcd8f8cbbcb01db17ca195a76006d4f3c5f94609
Bartosz Golab <golabwebdev@gmail.com>
2024-10-15 19:48:11 +0000

    test scrap site

---
+++ resources/js/Pages/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Menu Restauracji</h1>
+      <ul v-if="menu.length">
+        <li v-for="item in menu" :key="item.name">
+          {{ item.name }} - {{ item.price }}
+        </li>
+      </ul>
+      <p v-else>Nie udało się załadować menu lub jest puste.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      menu: {
+        type: Array,
+        required: true,
+        default: () => [],
+      }
+    }
+  };
+  </script>
