6187a8c794280b2a95522c46be8e7d951f0b5d2f
Golab <golabwebdev@gmail.com>
2024-10-17 08:33:28 +0000

    background update

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,9 +1,11 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Background from '@/Components/Background.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
+    <Background/>
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
     >
