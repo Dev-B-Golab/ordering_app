78995d96ffe2d9d30e81e10f1a56a5a2b22f2936
Golab <golabwebdev@gmail.com>
2024-10-18 05:27:30 +0000

    navbar in autenticatedLayout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,143 +1,70 @@
 <script setup>
-import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
+// import { ref } from 'vue';
+// import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+// import Dropdown from '@/Components/Dropdown.vue';
+// import DropdownLink from '@/Components/DropdownLink.vue';
+// import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+// import { Link } from '@inertiajs/vue3';
 import Background from '@/Components/Background.vue';
 
 
-const showingNavigationDropdown = ref(false);
+// const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
     <div>
         <Background/>
-        <div class="">
-            <nav class="navbar navbar-expand-lg bg-primary rounded">
-                <div class="container-fluid">
-                    <div class="">
-                        <div class="">
-
-                            <!-- Navigation Links -->
-                            <div
-                                class=""
-                            >
-                                <NavLink
-                                    :href="route('dashboard')"
-                                    :active="route().current('dashboard')"
-                                    class="btn btn-danger"
-                                >
-                                    Dashboard
-                                </NavLink>
-                            </div>
-                        </div>
-
-                        <div class="">
-                            <!-- Settings Dropdown ??????????--> 
-                            <div class="">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="i">
-                                            <button
-                                                type="button"
-                                                class=""
-                                            >
-                                                {{ $page.props.auth.user.name }}
-
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink
-                                            :href="route('profile.edit')"
-                                        >
-                                            Profile
-                                        </DropdownLink>
-                                        <DropdownLink
-                                            :href="route('logout')"
-                                            method="post"
-                                            as="button"
-                                        >
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                      
-                        
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                <div
-                    :class="{
-                        block: showingNavigationDropdown,
-                        hidden: !showingNavigationDropdown,
-                    }"
-                    class=""
-                >
-                    <div class="">
-                        <ResponsiveNavLink
-                            :href="route('dashboard')"
-                            :active="route().current('dashboard')"
-                        >
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div
-                        class=""
-                    >
-                        <div class="">
-                            <div
-                                class=""
-                            >
-                                {{ $page.props.auth.user.name }}
-                            </div>
-                            <div class="">
-                                {{ $page.props.auth.user.email }}
-                            </div>
-                        </div>
-
-                        <div class="">
+        <nav class="navbar navbar-expand-md">
+            <div class="container-fluid">
+                <ResponsiveNavLink href="/"
+                            class="navbar-brand">
+                             <h3>Zamawia-MY</h3>
+                            </ResponsiveNavLink>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                    <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Zamówienie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Historia zamówień</a>
+                        </li>
+                        <li class="nav-item">
                             <ResponsiveNavLink :href="route('profile.edit')"
-                            class="btn btn-danger">
-                                Profile
+                            class="nav-link">
+                                Profil
                             </ResponsiveNavLink>
-                            <ResponsiveNavLink
-                                :href="route('logout')"
-                                class="btn btn-danger"
-                                method="post"
-                                as="button"
-                            >
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
+                        </li>
+                        <li class="nav-item">
+           
+                        </li>
+                    </ul>
+                    <ResponsiveNavLink
+                            :href="route('logout')"
+                            class="nav-link"
+                            method="post"
+                            as="button"
+                        >
+                            Wyloguj
+                        </ResponsiveNavLink>
                 </div>
-            </nav>
-
-            <!-- Page Heading -->
-            <header
-                class=""
-                v-if="$slots.header"
-            >
-                <div class="">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
-        </div>
+            </div>
+        </nav>
+        <!-- Page Heading -->
+        <header
+            class=""
+            v-if="$slots.header"
+        >
+            <div class="">
+                <slot name="header" />
+            </div>
+        </header>
+        <!-- Page Content -->
+        <main>
+            <slot />
+        </main>
     </div>
 </template>
